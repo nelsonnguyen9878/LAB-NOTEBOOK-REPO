4166157e435c70e9d196c703417ea523a1c79fed
Nelson Nguyen <nelsonnguyen@Mac.lan>
2026-05-25 01:11:30 -0700

    Javascript

--- 06-javascript/02-arrays.js
+++ 06-javascript/02-arrays.js
@@ -1,29 +1,35 @@
 const pokemons = [
-  { id: 1, name: 'Bulbasaur', types: ['Grass', 'Poison'] },
-  { id: 2, name: 'Ivysaur', types: ['Grass', 'Poison'] },
-  { id: 3, name: 'Venusaur', types: ['Grass', 'Poison'] },
-  { id: 4, name: 'Charmander', types: ['Fire'] },
-  { id: 5, name: 'Charmeleon', types: ['Fire'] },
-  { id: 6, name: 'Charizard', types: ['Fire', 'Flying'] },
-  { id: 7, name: 'Squirtle', types: ['Water'] },
-  { id: 8, name: 'Wartortle', types: ['Water'] },
-  { id: 9, name: 'Blastoise', types: ['Water'] },
-  { id: 10, name: 'Caterpie', types: ['Bug'] },
-  { id: 11, name: 'Metapod', types: ['Bug'] },
-  { id: 12, name: 'Butterfree', types: ['Bug', 'Flying'] },
-  { id: 13, name: 'Weedle', types: ['Bug', 'Poison'] },
-  { id: 14, name: 'Kakuna', types: ['Bug', 'Poison'] },
-  { id: 15, name: 'Beedrill', types: ['Bug', 'Poison'] },
-  { id: 16, name: 'Pidgey', types: ['Normal', 'Flying'] },
-  { id: 17, name: 'Pidgeotto', types: ['Normal', 'Flying'] },
-  { id: 18, name: 'Pidgeot', types: ['Normal', 'Flying'] },
-  { id: 19, name: 'Rattata', types: ['Normal'] },
-  { id: 20, name: 'Raticate', types: ['Normal'] },
+  { id: 1, name: "Bulbasaur", types: ["Grass", "Poison"] },
+  { id: 2, name: "Ivysaur", types: ["Grass", "Poison"] },
+  { id: 3, name: "Venusaur", types: ["Grass", "Poison"] },
+  { id: 4, name: "Charmander", types: ["Fire"] },
+  { id: 5, name: "Charmeleon", types: ["Fire"] },
+  { id: 6, name: "Charizard", types: ["Fire", "Flying"] },
+  { id: 7, name: "Squirtle", types: ["Water"] },
+  { id: 8, name: "Wartortle", types: ["Water"] },
+  { id: 9, name: "Blastoise", types: ["Water"] },
+  { id: 10, name: "Caterpie", types: ["Bug"] },
+  { id: 11, name: "Metapod", types: ["Bug"] },
+  { id: 12, name: "Butterfree", types: ["Bug", "Flying"] },
+  { id: 13, name: "Weedle", types: ["Bug", "Poison"] },
+  { id: 14, name: "Kakuna", types: ["Bug", "Poison"] },
+  { id: 15, name: "Beedrill", types: ["Bug", "Poison"] },
+  { id: 16, name: "Pidgey", types: ["Normal", "Flying"] },
+  { id: 17, name: "Pidgeotto", types: ["Normal", "Flying"] },
+  { id: 18, name: "Pidgeot", types: ["Normal", "Flying"] },
+  { id: 19, name: "Rattata", types: ["Normal"] },
+  { id: 20, name: "Raticate", types: ["Normal"] },
 ];
 
 // Add your code here for: forEachPokemon
-
-console.group('=========== forEachPokemon =========== ');
+const forEachPokemon = function () {
+  pokemons.forEach((pokemon) => {
+    console.log(
+      `#${pokemon.id} ${pokemon.name} - ${pokemon.types.join(" / ")}`
+    );
+  });
+};
+console.group("=========== forEachPokemon =========== ");
 console.log(forEachPokemon());
 // #1 Bulbasaur - Grass / Poison
 // #2 Ivysaur - Grass / Poison
@@ -49,23 +55,38 @@ console.groupEnd();
 
 // Add your code here for: filterPokemons
 
-console.group('=========== filterPokemons =========== ');
-console.log(filterPokemons('Fire'));
+const filterPokemons = function (type) {
+  return pokemons
+    .filter((pokemon) => pokemon.types.includes(type))
+    .map((pokemon) => pokemon.name)
+    .sort();
+};
+
+console.group("=========== filterPokemons =========== ");
+console.log(filterPokemons("Fire"));
 // [ 'Charizard', 'Charmander', 'Charmeleon' ]
-console.log(filterPokemons('Normal'));
+console.log(filterPokemons("Normal"));
 // [ 'Pidgeot', 'Pidgeotto', 'Pidgey', 'Raticate', 'Rattata' ]
-console.log(filterPokemons('Poison'));
+console.log(filterPokemons("Poison"));
 // [ 'Beedrill', 'Bulbasaur', 'Ivysaur', 'Kakuna', 'Venusaur', 'Weedle' ]
 console.groupEnd();
 
 // Add your code here for: searchPokemons
-
-console.group('=========== searchPokemons =========== ');
-console.log(searchPokemons('Wartortle'));
+const searchPokemons = function (search) {
+  lowerSearch = search.toLowerCase();
+  return pokemons.filter((pokemon) => {
+    return (
+      pokemon.name.toLowerCase() === lowerSearch ||
+      pokemon.types.some((type) => type.toLowerCase() === lowerSearch)
+    );
+  });
+};
+console.group("=========== searchPokemons =========== ");
+console.log(searchPokemons("Wartortle"));
 // [ { id: 8, name: 'Wartortle', types: [ 'Water' ] } ]
-console.log(searchPokemons('pidgey'));
+console.log(searchPokemons("pidgey"));
 // [ { id: 16, name: 'Pidgey', types: [ 'Normal', 'Flying' ] } ]
-console.log(searchPokemons('bug'));
+console.log(searchPokemons("bug"));
 // [
 //   { id: 10, name: 'Caterpie', types: [ 'Bug' ] },
 //   { id: 11, name: 'Metapod', types: [ 'Bug' ] },
@@ -77,8 +98,19 @@ console.log(searchPokemons('bug'));
 console.groupEnd();
 
 // Add your code here for: reducePokemons
+const reducePokemons = pokemons.reduce((accumulator, pokemon) => {
+  pokemon.types.forEach((type) => {
+    if (accumulator[type]) {
+      accumulator[type]++;
+    } else {
+      accumulator[type] = 1;
+    }
+  });
 
-console.group('=========== reducePokemons =========== ');
+  return accumulator;
+}, {});
+
+console.group("=========== reducePokemons =========== ");
 console.log(reducePokemons);
 // {
 //   Grass: 3,
--- 06-javascript/03-age.js
+++ 06-javascript/03-age.js
@@ -1,14 +1,44 @@
 // Add your code here
+const calculateAge = function (birthDate) {
+  const today = new Date("2026-5-18");
+  const date = new Date(birthDate);
 
-console.log(calculateAge('2000-07-01'));
+  if (isNaN(date)) {
+    return "Error: Invalid date format";
+  }
+
+  if (date > today) {
+    return "Error: Birthdate cannot be in the future";
+  }
+
+  let age = today.getFullYear() - date.getFullYear();
+
+  const monthDiff = today.getMonth() - date.getMonth();
+
+  if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < date.getDate())) {
+    age--;
+  }
+
+  if (age < 0) {
+    return "Error: Invalid date format";
+  }
+
+  if (age > 125) {
+    return "Are you sure you are more than 125 years old?";
+  }
+
+  return `You are ${age} years old`;
+};
+
+console.log(calculateAge("2000-07-01"));
 // You are 25 years old
-console.log(calculateAge('1988-05-18'));
+console.log(calculateAge("1988-05-18"));
 // You are 38 years old
-console.log(calculateAge('2190-01-01'));
+console.log(calculateAge("2190-01-01"));
 // Error: Birth date cannot be in the future
-console.log(calculateAge('1800-01-01'));
+console.log(calculateAge("1800-01-01"));
 // Are you sure you are more than 125 years old?
-console.log(calculateAge('invalid-date'));
+console.log(calculateAge("invalid-date"));
 // Error: Invalid date format
 
 // Note: These calculations were done on May 18, 2026.
